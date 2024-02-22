ee2a6df14fba762e7b8853512af64fb9b32ddec4
Vanisco Tadiha <tvanisco@gmail.com>
2024-02-22 01:34:09 +0100

    ajouter du deuxieme test pour le score

--- Node/test.js
+++ Node/test.js
@@ -5,15 +5,24 @@ import app from './index.js';
 chai.use(chaiHttp);
 const { expect } = chai;
 
-describe('Content Moderation API', () => {
-  it('should predict moderation correctly', async () => {
+describe('Test de moderation', () => {
+  it('moderation pour la prediction', async () => {
     const res = await chai
       .request(app)
       .post('/api/moderation/predict')
-      .send({ text: 'Contenu à modérer', language: 'fr-FR' });
+      .send({ text: 'les pirates le mal la marine la justice', language: 'fr-FR' });
        expect(res).to.have.status(200);
        expect(res.body).to.have.property('prediction');
        expect(res.body.prediction[0]).to.be.a('number');
+  });
+  it('moderation pour le score', async () => {
+    const res = await chai
+      .request(app)
+      .post('/api/moderation/score')
+      .send({ text: 'today we are going to talk about IT', language: 'en-EN' });
+       expect(res).to.have.status(200);
+       expect(res.body).to.have.property('score');
+       expect(res.body.score).to.be.a('number');
   });
 });
 
